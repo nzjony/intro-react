d1f0491607799d545f3d6a886820d1786a9d953c
Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>
2021-05-21 22:28:15 +0200

    uncomment assignment rendering
    Signed-off-by: Jonathan Stichbury <2533428+nzjony@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,7 @@ class App extends React.Component {
     let tabChoice = <div />;
 
     /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -59,7 +59,7 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
